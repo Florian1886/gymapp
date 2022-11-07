93f4993f146d7b418a5da8b0371495290b6fe55c
Bartosz Florkiewicz <florek518@gmail.com>
2022-11-07 15:45:29 +0100

    initial commit

---
+++ src/test/java/pl/florian1886/gymapp/GymappApplicationTests.java
@@ -0,0 +1,13 @@
+package pl.florian1886.gymapp;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class GymappApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
